54095b66d429bbeb61299a9ae379ae931ae5aa09
Charlie Salcedo <salcedo.char0311@gmail.com>
2023-09-11 01:05:03 -0400

    lab done

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+// `saturdayFun` function
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  // `mondayWork` function expression
+  const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  };
+  
+  // `wrapAdjective` function
+  function wrapAdjective(emphasis = "*") {
+    return function(adjective) {
+      return `You are ${emphasis}${adjective}${emphasis}!`;
+    };
+  }
+  
+  module.exports = { saturdayFun, mondayWork, wrapAdjective };
+  
